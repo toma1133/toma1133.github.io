42e2020e31094de95bcaf3e6a6b89a987a428ad0
NickSu <6756536+toma1133@users.noreply.github.com>
2025-01-12 19:42:04 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = 'toma1133.github.io';
+}
